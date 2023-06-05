e85258abbae70f23d98769176c0691a1b32e9483
Pierce Ortlieb <pierce.ortlieb@fcagroup.com>
2023-06-05 09:59:58 -0400

    Letting CoPilot do its thing for me!

--- firstFile.py
+++ firstFile.py
@@ -1,1 +1,22 @@
 # First commit
+# All functions below were created by CoPilot! 
+
+# I need a function that calculates area of a circle
+
+def areaOfCircle(radius):
+    return 3.14 * radius * radius
+
+# I need a function that calculates area of a square
+
+def areaOfSquare(side):
+    return side * side
+
+# I need a function that calculates area of a rectangle
+
+def areaOfRectangle(length, width):
+    return length * width
+
+# I need a function that calculates area of a triangle
+
+def areaOfTriangle(base, height):
+    return 0.5 * base * height
